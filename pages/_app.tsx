622ebf3af0aac986c2813b64389304a2c3b757ea
Bernd von Platen <bernd.von.platen@gmail.com>
2023-11-17 12:34:07 +0100

    start with layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from 'next/app';
 import { DataProvider } from '../context/DataContext';
 import styles from '../styles/Home.module.css';
+import Layout from '../components/Layout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-        <DataProvider>
-        <Component {...pageProps} />
-        </DataProvider>
+      <DataProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </DataProvider>
     </>
   );
 }
